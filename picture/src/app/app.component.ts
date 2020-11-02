55f262547a273402e44f3ed240ce8bff168ee69d
Gian Dal Pont <giandalpont@gmail.com>
2020-11-02 14:37:27 -0300

    criado component photo

--- picture/src/app/app.component.ts
+++ picture/src/app/app.component.ts
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gian Dal Pont';
-  img = 'https://portosaopedro.com.br/wp-content/uploads/2019/03/aproximacao-da-natureza-promove-qualidade-de-vida-1.jpg';
-  imgAlt = 'Natureza';
 }
---
+++ picture/src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'ap-photo',
+  templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+  img = 'https://portosaopedro.com.br/wp-content/uploads/2019/03/aproximacao-da-natureza-promove-qualidade-de-vida-1.jpg';
+  imgAlt = 'Natureza';
+}
