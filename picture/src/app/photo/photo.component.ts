9c005df54ada685f82cbbe1766ba5bf2bc072e2c
Gian Dal Pont <giandalpont@gmail.com>
2020-11-02 14:56:38 -0300

    Inbound properties

--- picture/src/app/photo/photo.component.ts
+++ picture/src/app/photo/photo.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'ap-photo',
   templateUrl: 'photo.component.html'
 })
 export class PhotoComponent {
-  img = 'https://portosaopedro.com.br/wp-content/uploads/2019/03/aproximacao-da-natureza-promove-qualidade-de-vida-1.jpg';
-  imgAlt = 'Natureza';
+  // img = 'https://portosaopedro.com.br/wp-content/uploads/2019/03/aproximacao-da-natureza-promove-qualidade-de-vida-1.jpg';
+  // imgAlt = 'Natureza';
+  @Input() img = '';
+  @Input() imgAlt = '';
 }
