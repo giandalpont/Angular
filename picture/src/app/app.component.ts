ef1c3baa0e2363133ee54d7690cbca36f59bc102
Gian Dal Pont <giandalpont@gmail.com>
2020-11-02 18:38:30 -0300

    Isolando a acesso em serviços

--- picture/src/app/app.component.ts
+++ picture/src/app/app.component.ts
@@ -1,22 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Gian Dal Pont';
 
   photos: Object[] = [];
 
-  constructor(http: HttpClient) {
-    http
-      .get<Object[]>('http://localhost:3000/flavio/photos')
-      .subscribe(
-        photos => this.photos = photos,
-        err => console.log(err.message)
-      );
+  // constructor(http: HttpClient) {
+  //   http
+  //     .get<Object[]>('http://localhost:3000/flavio/photos')
+  //     .subscribe(
+  //       photos => this.photos = photos,
+  //       err => console.log(err.message)
+  //     );
+  // }
+
+  constructor(photoService: PhotoService) {
+    photoService
+      .listFromUser('flavio')
+      .subscribe(photos => this.photos = photos)
   }
 }
