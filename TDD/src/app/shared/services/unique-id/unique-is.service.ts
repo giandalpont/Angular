5ef0cd3af8c6f6737229fc25e9898d85b6ac0c5b
Gian Dal Pont <gian.pont@hneducacional.com.br>
2021-11-28 23:10:25 -0300

    service uuid

---
+++ TDD/src/app/shared/services/unique-id/unique-is.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class UniqueIdService {
+  private numberOfGeneratedIds = 0;
+
+  public generateUniqueIdWithPrefix(prefix: string): string {
+    if (prefix) {
+      throw Error('O prefixo não pode estar vazio');
+    }
+    const uniqueId = this.generateUniqueId();
+    this.numberOfGeneratedIds++;
+    return `${prefix}-${uniqueId}`;
+  }
+
+  public getNumberOfGeneratedIds(): number {
+    return this.numberOfGeneratedIds;
+  }
+
+  private generateUniqueId(): string {
+    return uuidv4();
+  }
+}
